ac9d9b32e56ebb1a9748e8dfdca8ea6af06e90fb
yorbenGoor123 <yorbengoor5@gmail.com>
2020-07-25 11:58:40 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './sections/Header/Header';
+import SectionInfo from './sections/SectionInfo/SectionInfo';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
