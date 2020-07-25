9807226fc89423339153b1ec2a8cbfd207b25f4e
yorbenGoor123 <yorbengoor5@gmail.com>
2020-07-25 15:28:43 +0200

    setting up firebase

---
+++ src/stores/RootStore.js
@@ -0,0 +1,27 @@
+import * as firebase from "firebase/app";
+import { configure } from 'mobx';
+
+
+configure({enforceActions: "observed"});
+
+class RootStore {
+    constructor() {
+        var firebaseConfig = {
+            apiKey: process.env.REACT_APP_apiKey,
+            authDomain: process.env.REACT_APP_authDomain,
+            databaseURL: process.env.REACT_APP_databaseURL,
+            projectId: process.env.REACT_APP_projectId,
+            storageBucket: process.env.REACT_APP_storageBucket,
+            messagingSenderId: process.env.REACT_APP_messagingSenderId,
+            appId: process.env.REACT_APP_appId
+          };
+        // Initialize Firebase
+        this.firebase = firebase.initializeApp(firebaseConfig);
+    }
+}
+
+const rootStore = new RootStore();
+
+export {rootStore}
+
+export default RootStore;
