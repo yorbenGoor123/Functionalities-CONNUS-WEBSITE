4f0783c09638f45f8fdbdd52541128de00f60bc9
yorbenGoor123 <yorbengoor5@gmail.com>
2020-07-26 18:04:13 +0200

    landing detailPage

--- src/pages/detail/DetailFunctionalities.js
+++ src/pages/detail/DetailFunctionalities.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import style from './DetailFunctionalities.module.css'
 import HeaderFunctionalities from '../../sections/HeaderFunctionalities/HeaderFunctionalities';
-
+import HeaderImage from '../../assets/detailpages/01-content-creator/headerImage.svg'
 const DetailFunctionalities = () => {
     return (
         <div className={style.detailWrapper}>
            <HeaderFunctionalities />
-           <p>Detail page functionalities</p>
+           
+           <div className={style.detailPageLanding}>
+               <div className={style.detailPageLanding__info}>
+                    <p className={style.detailPageLanding__info__number}>01</p>
+                    <p className={style.detailPageLanding__info__title}>Professional network</p>
+               </div>
+                <img className={style.detailPageLanding__headerImage} src={HeaderImage} alt="headerImage"></img>
+           </div>
+
+           
+            <ul className={style.detailPage__MenuItems}>
+                <li className={style.detailPage__MenuItem}>What are other content creators up to?</li>
+                <li className={style.detailPage__MenuItem}>Connect with the world. </li>
+                <li className={style.detailPage__MenuItem}>Share tips, tricks, and insights.</li>
+                <li className={style.detailPage__MenuItem}>On the road or at your desk?</li>
+            </ul>
+           
         </div>
     );
 };
